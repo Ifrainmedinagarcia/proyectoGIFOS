d4e53b3066567234c1a6382d0e58e5fb99dde158
Ifrain <ifrain.audiovisual@gmail.com>
2020-11-15 21:54:19 -0300

    darkMode and localStorage ready

--- js/main.js
+++ js/main.js
@@ -20,5 +20,16 @@ let darkMode = document.getElementById('darkMode');
 darkMode.addEventListener('click', (e) =>{
     e.preventDefault();
     document.body.classList.toggle('dark');
+    //Guardamos el modo en localStorage
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('dark-mode', 'true');
+    }else{
+        localStorage.setItem('dark-mode', 'false');
+    }
 });
+if (localStorage.getItem('dark-mode')=== 'true') {
+    document.body.classList.add('dark');
+}else{
+    document.body.classList.remove('dark');
+}
 /* Dark mode */
