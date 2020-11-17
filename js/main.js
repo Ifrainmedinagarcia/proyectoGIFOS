642e02868b625c7532627e1a3ac02e359d03d55d
Ifrain <ifrain.audiovisual@gmail.com>
2020-11-16 22:42:35 -0300

    api trending, function create html dynamic

--- js/main.js
+++ js/main.js
@@ -15,9 +15,7 @@ const funcionalidadMenuClose = () =>{
 menuBurguer.addEventListener('click', funcionalidadMenuOpen);
 menuBurguerClose.addEventListener('click', funcionalidadMenuClose);
 /* Funcionalidad de Menú hamburguesa */
-/* Hover of Create GIFOS */
 
-/* Hover of Create GIFOS */
 /* Dark mode */
 let darkMode = document.getElementById('darkMode');
 darkMode.addEventListener('click', (e) =>{
@@ -36,6 +34,7 @@ if (localStorage.getItem('dark-mode') === 'true') {
     document.body.classList.remove('dark');
 }
 /* Dark mode */
+
 /* Carrusel trending */
 const fila = document.querySelector('.container_trending_gifos_flex');
 const arrowLef = document.getElementById('arrow-lef');
@@ -49,5 +48,33 @@ arrowLef.addEventListener('click', ()=>{
 /* Carrusel trending */
 
 /* API Trending */
+const api = 'https://api.giphy.com/v1/gifs/trending?api_key=pEo6WXMy7qnbv6QjNIWmRF2ghCLUvM4L&limit=25&rating=g'
 
+const createDom = () =>{
+    let containerTrending = document.getElementById('containerTrending');
+    let gifosTrending = document.createElement('div');
+    gifosTrending.classList.add('gifos_trending');
+
+    let infoGif = document.createElement('div');
+    infoGif.classList.add('info_gif');
+
+    let actionsUser = document.createElement('div');
+    actionsUser.classList.add('actions_users');
+
+    let iconFav = document.createElement('div');
+    iconFav.classList.add('icon_fav', 'tamaño_actions_users');
+    actionsUser.appendChild(iconFav);
+
+    let iconDownload = document.createElement('div');
+    iconDownload.classList.add('icon_download', 'tamaño_actions_users');
+    actionsUser.appendChild(iconDownload);
+
+    let iconMax = document.createElement('div');
+    iconMax.classList.add('icon_max', 'tamaño_actions_users');
+    actionsUser.appendChild(iconMax);
+
+    infoGif.appendChild(actionsUser);
+    gifosTrending.appendChild(infoGif);
+    containerTrending.appendChild(gifosTrending);
+}
 /* API Trending */
