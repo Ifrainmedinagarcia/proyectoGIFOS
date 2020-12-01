e62ac640c0e53c255f70f86237c2fee4a784e744
Ifrain <ifrain.audiovisual@gmail.com>
2020-12-01 16:23:19 -0300

    start function show more

--- js/search.js
+++ js/search.js
@@ -7,12 +7,13 @@ const showMore = document.querySelector('.showMore');
 const suggestionsContainer = document.getElementById('suggestions');
 const lineSuggestions = document.querySelector('.line_search_input');
 const xCancelSearch = document.querySelector('.x_cancel_search');
+const h1Title = document.getElementById('title');
+const lupa = document.querySelector('.fa-search');
 
 const search = (title) =>{
     const urlSearch = `${baseApi}search?api_key=${apiSearch}&q=${title}&q=&limit=12`;
     const result = getIfoApi(urlSearch);
     searchResult.innerHTML = '';
-    suggestionsContainer.innerHTML = '';
     result.then((resp)=>{
         resp.data.map((items, i) => domSearch(items, i));
     }).catch((e) => {
@@ -20,15 +21,15 @@ const search = (title) =>{
     });
 }
 const domSearch = (gifSearch, i) =>{
-    const h1Title = document.getElementById('title');
     h1Title.textContent = searchValue.value;
     createDom(gifSearch, searchResult, i);
 }
-searchBtn.addEventListener('click', ()=>{
+lupa.addEventListener('click', ()=>{
     if (searchValue.value != '') {
         search(searchValue.value);
-        showMore.classList.remove('none');
         lineSuggestions.classList.add('none');
+        showMore.classList.remove('none');
+        suggestionsContainer.innerHTML = '';
     }else{
         alert('Por favor introduce un término de búsqueda');
     }
@@ -38,8 +39,7 @@ searchValue.addEventListener('keyup', () => {
         if (searchValue.value != '') {
             search(searchValue.value);
             showMore.classList.remove('none');
-            lineSuggestions.classList.add('none');
-            
+            lineSuggestions.innerHTML = '';
         }else{
             alert('Por favor introduce un término de búsqueda');
         }
@@ -48,7 +48,6 @@ searchValue.addEventListener('keyup', () => {
 /* API Search */
 
 /* Buscador con sugerencia */
-
 const suggestions = (title) =>{
     const urlSuggestion = `${baseApi}search?api_key=${apiSearch}&q=${title}&q=&limit=8`;
     const result = getIfoApi(urlSuggestion);
@@ -64,38 +63,60 @@ const suggestions = (title) =>{
         console.log("a ocurrido un error " + e);
     });
 }
-
 const filtrar = () =>{
     const texto = searchValue.value;
     suggestions(texto);
 }
-
 const suggestionsDom = (sugerencia) =>{
     const divContainerPSug = document.createElement('div');
     divContainerPSug.classList.add('sugerencia');
+    const lupa = document.createElement('i');
+    lupa.classList.add('fas', 'fa-search', 'lupaSuggestions');
 
+    
     const pSugerencia = document.createElement('p');
     pSugerencia.classList.add('sugerenciaP');
     pSugerencia.textContent = sugerencia.title;
+    pSugerencia.appendChild(lupa);
+
 
     divContainerPSug.appendChild(pSugerencia);
     divContainerPSug.addEventListener('click', () =>{
         search(sugerencia.title);
+        h1Title.textContent = sugerencia.title;
+        searchValue.value = sugerencia.title;
         lineSuggestions.classList.add('none');
         showMore.classList.remove('none');
         suggestionsContainer.innerHTML = '';
     });
+    
     suggestionsContainer.appendChild(divContainerPSug);
 };
-
 searchValue.addEventListener('keyup', ()=>{
-    filtrar();
     if (searchValue.value !== '') {
+        filtrar();
+        lupa.classList.remove('none');
         lineSuggestions.classList.remove('none');
+        xCancelSearch.classList.remove('none');
+        xCancelSearch.addEventListener('click', () =>{
+            searchValue.value = '';
+            lupa.classList.add('none');
+            searchBtn.classList.remove('none');
+            xCancelSearch.classList.add('none');
+            suggestionsContainer.innerHTML = '';
+        });
+        searchBtn.classList.add('none');
     }else{
+        lupa.classList.add('none');
         xCancelSearch.classList.add('none');
+        searchBtn.classList.remove('none')
+        suggestionsContainer.innerHTML = '';
     }
 } );
-
-
 /* Buscador con sugerencia */
+
+/* Show More */
+/* showMore.addEventListener('click', () =>{
+    search(h1Title.textContent);
+}); */
+/* Show More */
