dfd9bd48bf6536f214ffb3b6a2ecd66833b449cc
Ifrain <ifrain.audiovisual@gmail.com>
2020-12-09 21:33:24 -0300

    retornando cambio

--- js/favorite.js
+++ js/favorite.js
@@ -11,14 +11,13 @@ const createDomFavorite = () => {
     if (gifLocal.length <= 0) {
         favoriteClean.classList.remove('none');
         gifosContainerFlex.innerHTML = '';
-    }
-    
+    } else {
         favoriteClean.classList.add('none');
         gifosContainerFlex.innerHTML = '';
         gifLocal.forEach((favoritoGif, index) => {
             createDom(favoritoGif, gifosContainerFlex, index);
         });
-
+    };
 }
 createDomFavorite();
 /* Add to favorite */
